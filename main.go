7f5291baf83c4416fb47515c254115b1d845e5e8
smeghead <smeghead7@gmail.com>
2011-06-02 19:01:38 +0900

    start up firefox.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"time"
 	"http"
 	"io"
 	"log"
@@ -24,16 +25,37 @@ import (
  *  - kill firefox.
  *  - kill virtual display.
  */
+const MAX_VD = 1
 
-func RunCommand(command string, display int) {
-	environ := os.Environ()
-	environ = append(environ, fmt.Sprintf("DISPLAY=%d:0", display))
-	var args []string
-	args = append(args, "")
-	args = append(args, "-l")
-	args = append(args, "/home")
+func InitVirtualScreen() {
+	for i := 0; i < MAX_VD; i++ {
+		environ := os.Environ()
+		environ = append(environ, fmt.Sprintf("DISPLAY=:%d.0", i + 1))
+		// Xvfbの起動
+		go func (n int) {
+			log.Print(n);
+			log.Print(environ);
+			command := "/usr/bin/Xvfb"
+			args := []string {"Xvfb", fmt.Sprintf(":%d", n + 1), "-screen", "0", "1024x768x24"}
+			RunCommand(command, args, environ)
+		}(i)
+		time.Sleep(3000000000)
+		// Firefoxの起動
+		go func (n int) {
+			log.Print(n);
+			log.Print(environ);
+			command := "/usr/bin/firefox"
+			args := []string {"firefox", "-display", fmt.Sprintf(":%d", n + 1), "-width", "1024", "-height", "800"}
+			RunCommand(command, args, environ)
+		}(i)
+	}
+}
 
-	cmd, err := exec.Run(command, args, environ, ".", exec.DevNull, exec.Pipe, exec.Pipe)
+func RunCommand(command string, args []string, environ []string) {
+	log.Print(command)
+	log.Print(args)
+	cmd, err := exec.Run(command, args, environ, ".", exec.DevNull, exec.Pipe, exec.MergeWithStdout)
+	log.Print("Ran")
 	if err != nil {
 		log.Fatal(err)
 		log.Fatal("failed to execute external command.")
@@ -45,6 +67,7 @@ func RunCommand(command string, display int) {
 		log.Fatal("failed to execute external command.")
 		os.Exit(-1)
 	}
+	log.Print("--stdout--")
 	log.Print(string(b))
 }
 
@@ -54,10 +77,11 @@ func HelloServer(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	RunCommand("/bin/ls", 1);
-//	http.HandleFunc("/hello", HelloServer)
-//	err := http.ListenAndServe(":80", nil)
-//	if err != nil {
-//		log.Fatal("ListenAndServe: ", err.String())
-//	}
+	InitVirtualScreen()
+	//RunCommand("/bin/ls", 1);
+	http.HandleFunc("/hello", HelloServer)
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err.String())
+	}
 }
